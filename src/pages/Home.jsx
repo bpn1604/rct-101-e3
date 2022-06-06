7ccc4395e2f645479d6686b8220e1b1daadd335f
bpn1604 <bipin.kumar2015@vitalum.ac.in>
2022-06-06 17:06:01 +0530

    fin

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import Products from "../components/Products/Products"
 const Home = () => {
   return <div>
-    Bipin Home
+    
     <Products>
-      
+
     </Products>
   
   </div>;
